574d3a36b214e880723644805bb2883bce0441dc
Lorenzo Tomesani <loritome99@gmail.com>
2021-12-15 14:55:27 +0100

    commit4

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
     useEffect(() => {
         axios.get( baseUrl + "/exercises").then(res => {
-            console.log(res.data)
+            console.log(res)
             setExercises(res.data);
         }).catch(err => {
             console.log(err);
